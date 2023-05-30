f452cbd9bea1e6992293271d6505ca9913cc99d4
stphnVi <stephanie1117@hotmail.es>
2023-05-29 23:22:11 -0700

    prev rteces without concat

--- rteces.pl
+++ rteces.pl
@@ -1,16 +1,50 @@
+%ejemplos de lo que se espera de entradas
+
 :-consult("rtecbnf.pl").
 :-consult("rtecdb.pl").
 
 inicio():-
-    header(), 
-    datosUsuario().
+    header(),
+    tipoComida(), %pizza, ensalada, pasta..
+    datosPedido(), %napolitana, cesar, raviolis
+    tipoRefresco(), %Coca, papaya, horchata, pacha con jet
+    lugarHora(), %ubicacion y hora disponibles
+    cantidadPersonas(). % reservacion para x personas
 
 header():-
-    write("Bienvenidos a RestauranTEC, su bot de confianza para la recomendación de los mejores restaurantes del país.").
-
-datosUsuario() :-
-    %Consulta el tipo de comida que el usuario desea
-    nl, writeln('¿Qué tipo de comida desea comer?'), nl, nl, writeln("Por ejemplo: rapida, italiana, bocas, mexicana, etc...").
+    write("bienvenidos a restauranTEC, su bot de confianza para la recomendacion de los mejores restaurantes del pais.").
+    %writeln("Por ejemplo: rapida, italiana, bocas, mexicana, etc...").
     %parsear input de usuario
 
+tipoComida() :-
+    %Consulta el tipo de comida que el usuario desea
+    nl,
+    write('¿Que se te antoja comer? '), 
+    read(X),
+    %atomic.list.concat(L,' ',X),
+    %verificarOracion(X),
+    write(X).
 
+datosPedido() :-
+    nl,
+    write('¿Que tipo de X? '), 
+    read(X),
+    write(X).
+
+tipoRefresco() :-
+    nl,
+    write('¿Que tipo de refreso desea? '), 
+    read(X),
+    write(X).
+
+lugarHora() :-
+    nl,
+    write('¿A que hora y en que lugar se te antoja comer? '), 
+    read(X),
+    write(X).
+
+cantidadPersonas() :-
+    nl,
+    write('¿Para cuantas personas?'), 
+    read(X),
+    write(X).
