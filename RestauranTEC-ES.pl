3468429479004ca06ebcb64a72c693569cda2480
stphnVi <stephanie1117@hotmail.es>
2023-05-31 00:07:04 -0700

    documentation

--- RestauranTEC-ES.pl
+++ RestauranTEC-ES.pl
@@ -4,13 +4,17 @@
 inicio():-
     header(),
     usuario_input(Cadena),
-    usuario_input_aux(). %pizza, ensalada, pasta.
+    usuario_input_aux(). 
     
 
 header():-
     write("bienvenidos a restauranTEC, su bot de confianza para la recomendacion de los mejores restaurantes del pais.").
-    %writeln("Por ejemplo: rapida, italiana, bocas, mexicana, etc...").
-    %parsear input de usuario
+
+/*inicia la conversacion con el chat de recomendaciones. recibe un string del usuario
+y este se envia al archivo RestauranTEC-ID.pl para que la entrada sea procesada, hay dos caminos que 
+el usuario puede elegir, por recomendaciones directas o preguntas aleatorias para generar la recomendacion
+mas adecuada.
+*/
 
 usuario_input(Cadena) :-
     nl,
@@ -21,6 +25,12 @@ usuario_input(Cadena) :-
             recomendar();
             analizar_input(Cadena)),
             usuario_input_aux().
+
+/*En caso de que el usuario decida hacer preguntas aleatorias u oraciones en busca de 
+infomacion o recomendaciones se necesita una conversacion continua, para ello 
+usuario_input_aux() genera una llamda recursiva.
+
+*/
 
 usuario_input_aux() :-
     nl,
