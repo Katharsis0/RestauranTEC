61032f533dac320820234a732756735ca243a53a
stphnVi <stephanie1117@hotmail.es>
2023-05-30 18:33:33 -0700

    Fixing Interface to match with identify

--- RestauranTEC-ES.pl
+++ RestauranTEC-ES.pl
@@ -1,51 +1,23 @@
-:-consult("RestautanTEC-BNF.pl").
-:-consult("RestautanTEC-DB.pl").
+:-consult('RestauranTEC-ID.pl').
 
 
 inicio():-
     header(),
-    tipoComida(), %pizza, ensalada, pasta.
-    datosPedido(), %napolitana, cesar, raviolis
-    tipoRefresco(), %Coca, papaya, horchata, pacha con jet
-    lugarHora(), %ubicacion y hora disponibles
-    cantidadPersonas(). % reservacion para x personas
+    usuario_input(Cadena). %pizza, ensalada, pasta.
+    
 
 header():-
     write("bienvenidos a restauranTEC, su bot de confianza para la recomendacion de los mejores restaurantes del pais.").
     %writeln("Por ejemplo: rapida, italiana, bocas, mexicana, etc...").
     %parsear input de usuario
 
-tipoComida() :-
+usuario_input(Cadena) :-
     %Consulta el tipo de comida que el usuario desea
     nl,
-    write('¿Que se te antoja comer? '), 
-    read(X),
-    %atomic.list.concat(L,' ',X),
-    %verificarOracion(X),
-    write(X).
-datosPedido() :-
-    nl,
-    write('¿Que tipo de X? '), 
-    read(X),
-    write(X).
-
-tipoRefresco() :-
-    nl,
-    write('¿Que tipo de refreso desea? '), 
-    read(X),
-    write(X).
-
-lugarHora() :-
-    nl,
-    write('¿A que hora y en que lugar se te antoja comer? '), 
-    read(X),
-    write(X).
-
-cantidadPersonas() :-
-    nl,
-    write('¿Para cuantas personas?'), 
-    read(X),
-    write(X).
+    write('¿Que se te antoja comer? '),
+    read_line_to_codes(user_input, InputCodes),
+    atom_codes(Cadena, InputCodes),
+    analizar_input(Cadena).
 
 %parsear input de usuario
 
